7271e4473a6f80ee407ae81cb5a978830f1cc5cb
Jack Markley <jack.markley@gmail.com>
2014-03-31 23:19:22 +0200

    empty tests

--- resume_storage/tests.py
+++ resume_storage/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from models import Resume, Saved_Section, Saved_Entry
 
-# Create your tests here.
+
+class ResumeModelTestCase(TestCase):
+
+    def tearDown(self):
+        pass
+
+    def setUp(self):
+        pass
+
+    def testInsert(self):
+        pass
