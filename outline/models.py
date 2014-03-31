864b02743b81e09be2287143d3df13af66d719fa
risingmoon <justindavidlee88@gmail.com>
2014-03-31 14:38:24 -0700

    Premerge

--- outline/models.py
+++ outline/models.py
@@ -10,72 +10,55 @@ class Web(models.Model):
 
 class Header(models.Model):
     first_name = models.CharField(max_length=50)
-    middle_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, null=True)
     last_name = models.CharField(max_length=50)
-    cell = models.CharField(max_length=15)
-    home = models.CharField(max_length=15)
-    fax = models.CharField(max_length=15)
-    address1 = models.CharField(max_length=50)
-    address2 = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
-    zipcode = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    region = models.CharField(max_length=50)
-    web = models.ManyToManyField(Web)
+    cell = models.CharField(max_length=15, null=True)
+    home = models.CharField(max_length=15, null=True)
+    fax = models.CharField(max_length=15, null=True)
+    address1 = models.CharField(max_length=50, null=True)
+    address2 = models.CharField(max_length=50, null=True)
+    city = models.CharField(max_length=50, null=True)
+    state = models.CharField(max_length=50, null=True)
+    zipcode = models.CharField(max_length=50, null=True)
+    email = models.CharField(max_length=50, null=True)
+    region = models.CharField(max_length=50, null=True)
+    web = models.ManyToManyField(Web, null=True)
     user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return unicode(' '.join[
+            self.first_name,
+            self.middle_name,
+            self.last_name])
 
 
 class Section(models.Model):
     title = models.CharField(max_length=50)
-    description = models.CharField(max_length=50)
+    description = models.CharField(max_length=50, null=True)
     user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return unicode(self.title)
 
 
 class Entry(models.Model):
     title = models.CharField(max_length=50)
     subtitle = models.CharField(max_length=50, null=True)
+    start_date = models.DateTimeField(null=True)
+    end_date = models.DateTimeField(null=True)
     city = models.CharField(max_length=50, null=True)
     state = models.CharField(max_length=50, null=True)
     contact = models.CharField(max_length=50, null=True)
     description = models.CharField(max_length=50, null=True)
     section = models.ForeignKey('Section')
 
+    def __unicode__(self):
+        return unicode(self.title)
+
+    # def month_year(self):
+
+
 
 class Data(models.Model):
     text = models.CharField(max_length=400)
-
-# class Education(models.Model):
-#     certificate = models.CharField(max_length=50)
-#     city = models.CharField(max_length=50)
-#     state = models.CharField(max_length=50)
-#     organization = models.CharField(max_length=50)
-#     #Change
-#     date = models.CharField(max_length=50)
-#     description = models.CharField(max_length=50)
-#     person = models.ForeignKey(Person)
-
-
-# class Experience(models.Model):
-#     employer = models.CharField(max_length=50)
-#     organization = models.CharField(max_length=50)
-#     position = models.CharField(max_length=50)
-#     date = models.CharField(max_length=50)
-#     description = models.CharField(max_length=50)
-#     person = models.ForeignKey(Person)
-
-
-# class Portfolio(models.Model):
-#     name = models.CharField(max_length=50)
-#     description = models.CharField(max_length=50)
-#     person = models.ForeignKey(Person)
-
-
-# class Activities(models.Model):
-#     description = models.CharField(max_length=50)
-#     person = models.ForeignKey(Person)
-
-
-# class Interests(models.Model):
-#     description = models.CharField(max_length=50)
-#     person = models.ForeignKey(Person)
+    entry = models.ForeignKey('Entry')
--- outline/tests.py
+++ outline/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from outline.models import Photo, Album, Tag
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+
+class BasicTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            "test",
+            "test@example.com",
+            "test")
+        self.user.save()
+
+
+# class HeaderTest(self):
+
+#     def test_
