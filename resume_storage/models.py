8b3a226d7e297f8cca2c5cd0fd7d3a4b5cb49f5f
Jack Markley <jack.markley@gmail.com>
2014-04-01 21:46:05 +0200

    reworking models

--- resume_storage/models.py
+++ resume_storage/models.py
@@ -1,19 +1,47 @@
 from django.db import models
-from outline.models import Header, Section, Entry, Data
+from outline.models import Section, Entry, Data
 from django.contrib.auth.models import User
 
 
-# def get_user():
-#     return User.objects.get(pk=1)
+class Web(models.Model):
+    account = models.CharField(max_length=50)
 
 
 class Resume(models.Model):
+    #metadatas
     title = models.CharField(max_length=64)
-    header = models.OneToOneField(Header)
     user = models.ForeignKey(User)
 
-    def saveResume(self):
+    #header fields
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50)
+    cell = models.CharField(max_length=15, null=True)
+    home = models.CharField(max_length=15, null=True)
+    fax = models.CharField(max_length=15, null=True)
+    address1 = models.CharField(max_length=50, null=True)
+    address2 = models.CharField(max_length=50, null=True)
+    city = models.CharField(max_length=50, null=True)
+    state = models.CharField(max_length=50, null=True)
+    zipcode = models.CharField(max_length=50, null=True)
+    email = models.CharField(max_length=50, null=True)
+    region = models.CharField(max_length=50, null=True)
+    web = models.ManyToManyField(Web, null=True)
+
+    def setResumeFields(self):
         pass
+
+    def getResumeFields(self):
+        secEntDats = {}
+        for eachSection in Saved_Section.objects.filter(resume=self):
+            EntDats = {}
+            for eachEntry in Saved_Entry.objects.filter(section=eachSection):
+                Dats = []
+                for eachData in Data.objects.filter(entry=eachEntry):
+                    Dats.append(eachData)
+                EntDats[eachEntry] = Dats
+            secEntDats[eachSection] = EntDats
+        return self, secEntDats
 
 
 class Saved_Section(models.Model):
--- resume_storage/tests.py
+++ resume_storage/tests.py
@@ -1,7 +1,31 @@
 from django.test import TestCase
-from models import Resume, Saved_Section, Saved_Entry
+from outline.models import Section, Entry, Data
+from models import Resume, Web, Saved_Section, Saved_Entry
+from Django.contrib.auth.models import User
 
 
-class StorageModelsTestCase(TestCase):
+class ResumeStorageTestCase(TestCase):
 
-    pass
+    def setUp(self):
+        self.testUser = User.objects.create('testperson',
+                                            'fake@email.ro',
+                                            'badpass',)
+        Resume.objects.create(first_name='test',
+                              last_name='user',
+                              user=self.testUser)
+        for atitle in ('one title', 'two title', 'red title', 'blue title'):
+            asection = Section.objects.create(title=atitle, user=self.testUser)
+            for anothertitle in ('big title', 'small title', 'sub title'):
+                anentry = Entry.objects.create(title=anothertitle,
+                                               section=asection,)
+                for atext in ('almost', 'done'):
+                    Data.objects.create(text=atext)
+
+    def testGetResume(self):
+        newResume = Resume.objects.create(first_name='jack',
+                                          last_name='markley',
+                                          user=self.testUser)
+        self.assertEqual(newResume.getResumeFields(), (newResume, {}))
+
+    def testSetResume(self):
+        pass
