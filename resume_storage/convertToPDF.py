11e11db1ff509fa4e659f6a70fde8ca5cf941442
Jack Markley <jack.markley@gmail.com>
2014-04-02 22:22:12 +0200

    added reportlab to the requirements.txt

--- resume_storage/convertToPDF.py
+++ resume_storage/convertToPDF.py
@@ -4,6 +4,7 @@ from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.rl_config import defaultPageSize
 from reportlab.lib.units import inch
+from reportlab.pdfgen import canvas
 
 PAGE_HEIGHT = defaultPageSize[1]
 PAGE_WIDTH = defaultPageSize[0]
@@ -14,4 +15,8 @@ styles = getSampleStyleSheet()
 #and a file-like object, as outputFile
 #and writes a pretty-ish looking resume into the file as a pdf
 def writeResumePDF(resumeEntry, outputFile):
-    pass
+    filecanvas = canvas.Canvas(outputFile)
+    
+    filecanvas.showPage()
+    filecanvas.save()
+    return outputFile
