6caa9483b7cd44cac8bf8bc131b8cb89c81bd1bd
Jack Markley <jack.markley@gmail.com>
2014-04-02 21:31:10 +0200

    beginning the PDF conversion function

---
+++ resume_storage/convertToPDF.py
@@ -0,0 +1,17 @@
+from models import Resume, Resume_Web, Saved_Section, Saved_Entry
+from outline.models import Section, Entry, Data
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.rl_config import defaultPageSize
+from reportlab.lib.units import inch
+
+PAGE_HEIGHT = defaultPageSize[1]
+PAGE_WIDTH = defaultPageSize[0]
+styles = getSampleStyleSheet()
+
+
+#this function will take the database entry from Resume, as resumeEntry
+#and a file-like object, as outputFile
+#and writes a pretty-ish looking resume into the file as a pdf
+def writeResumePDF(resumeEntry, outputFile):
+    pass
