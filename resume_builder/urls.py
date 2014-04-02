3c3a3d18fcd04d02ba2354593abdacc071dff217
risingmoon <justindavidlee88@gmail.com>
2014-04-02 11:12:36 -0700

    Added static folder

--- resume_builder/urls.py
+++ resume_builder/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,4 +11,4 @@ urlpatterns = patterns(
     url(r'^resume_storage/', include('resume_storage.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
